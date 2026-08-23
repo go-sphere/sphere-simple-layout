2403fd4084abadf167b24245da44a577448afebf
tbxark <tbxark@outlook.com>
2026-08-24 00:44:59 +0800

    chore: Simplify config loading and require JWT
    Replace the select provider with a direct file provider and add a validation check that fails if the API JWT is empty.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,11 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/go-sphere/confstore"
 	"github.com/go-sphere/confstore/codec"
-	"github.com/go-sphere/confstore/provider"
 	"github.com/go-sphere/confstore/provider/file"
-	"github.com/go-sphere/confstore/provider/http"
 	"github.com/go-sphere/sphere-simple-layout/internal/server/api"
 	"github.com/go-sphere/sphere/log/zapx"
 	"github.com/go-sphere/sphere/utils/secure"
@@ -42,20 +42,15 @@ func NewEmptyConfig() *Config {
 }
 
 func NewConfig(path string) (*Config, error) {
-	config, err := confstore.Load[Config](provider.NewSelect(
-		path,
-		provider.If(file.IsLocalPath, func(s string) provider.Provider {
-			return file.New(path, file.WithExpandEnv())
-		}),
-		provider.If(http.IsRemoteURL, func(s string) provider.Provider {
-			return http.New(path, http.WithTimeout(10))
-		}),
-	), codec.JsonCodec())
+	config, err := confstore.Load[Config](file.New(path), codec.JsonCodec())
 	if err != nil {
 		return nil, err
 	}
 	if config.Log.Level == "" {
 		config.Log.Level = "info"
+	}
+	if config.API.JWT == "" {
+		return nil, fmt.Errorf("api jwt must be non-empty")
 	}
 	return config, nil
 }
